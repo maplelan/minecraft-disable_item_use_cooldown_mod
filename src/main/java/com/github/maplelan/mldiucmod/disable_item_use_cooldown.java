a0a4f667613fa2f31723945ca1d6d1709918ba9a
maplelan <maplelan030@gmail.com>
2023-01-15 02:32:25 +0800

    frist

---
+++ src/main/java/com/github/maplelan/mldiucmod/disable_item_use_cooldown.java
@@ -0,0 +1,13 @@
+package com.github.maplelan.mldiucmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(disable_item_use_cooldown.MODID)
+public class disable_item_use_cooldown{
+    public static final String MODID = "mldiucmod";
+    public static final String NAME = "disable item use cooldown";
+
+    public disable_item_use_cooldown(){
+    	
+    }
+}
